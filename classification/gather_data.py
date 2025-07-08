b3e3381c3f40a0706fc3dbac27c24ce211600b8e
William Christian <wllchrst@outlook.com>
2025-07-08 07:00:10 +0700

    feat optional removal column

--- classification/gather_data.py
+++ classification/gather_data.py
@@ -3,7 +3,7 @@ from datasets import load_dataset
 
 indo_qa_path = "jakartaresearch/indoqa"
 
-def gather_indo_qa() -> tuple[pd.DataFrame, pd.DataFrame]:
+def gather_indo_qa(remove_column: bool = True) -> tuple[pd.DataFrame, pd.DataFrame]:
     ds = load_dataset(indo_qa_path)
 
     train_df = pd.DataFrame(ds['train'])
@@ -11,7 +11,8 @@ def gather_indo_qa() -> tuple[pd.DataFrame, pd.DataFrame]:
     
     keep_column = ['id', 'question', 'answer', 'category']
 
-    test_df = test_df[keep_column]
-    train_df = train_df[keep_column]
+    if remove_column:
+        test_df = test_df[keep_column]
+        train_df = train_df[keep_column]
 
     return train_df, test_df
