b44984a932149c00b926f6091fbc9084da8f37e9
William Christian <wllchrst@outlook.com>
2026-03-10 18:32:59 +0700

    feat: add stable models to mode list

--- llm/ollama_llm.py
+++ llm/ollama_llm.py
@@ -3,7 +3,7 @@ from llm.base_llm import BaseLLM
 from helpers import env_helper
 from ollama import Client
 
-OLLAMA_MODEL_LIST = ['deepseek-r1:latest', 'bangundwir/bahasa-4b-chat', 'gemma3:latest', 'qwen3:8b']
+OLLAMA_MODEL_LIST = ['deepseek-r1:latest', 'bangundwir/bahasa-4b-chat', 'gemma3:latest', 'qwen3:8b', 'stable-qwen', 'stable-gemma']
 DEFAULT_TIMEOUT = 180
 DEFAULT_OPTIONS = {
     "temperature": 0.1,
